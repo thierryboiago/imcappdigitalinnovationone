7ff64ee3ce86861bb9eacf583342affbdd9ac220
Thierry Boiago <thierry123123@gmail.com>
2021-08-18 19:55:54 -0300

    aula de lifecycle

--- app/src/main/java/com/example/imc_app/MainActivity.kt
+++ app/src/main/java/com/example/imc_app/MainActivity.kt
@@ -2,11 +2,44 @@ package com.example.imc_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("hewllow")
+       Log.w("lifecycle","CREATE - estou criando a tela")
+        finish()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.w("lifecycle","CREATE - deixei a tela visível para você")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.w("lifecycle","RESUME - agora você pode interargir com a tela")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.w("lifecycle","PAUSE - a tela perdeu o foco,você não pode mais interagir")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.w("lifecycle","STOP - a tela não está mais visível mas ainda existe")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.w("lifecycle","RESTART - a tela está retomando o foco")
+    }
+
+    override fun onDestroy() {
+        Log.w("lifecycle","DESTROY - a tela foi destruida")
+        super.onDestroy()
+
     }
 }
