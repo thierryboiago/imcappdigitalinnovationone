3b0f4ce91027b235d07e1cdc84f68a6c35ce8015
Thierry Boiago <thierry123123@gmail.com>
2021-08-18 21:10:05 -0300

    finalizado aula da criação do app de imc

--- app/src/main/java/com/example/imc_app/MainActivity.kt
+++ app/src/main/java/com/example/imc_app/MainActivity.kt
@@ -3,13 +3,38 @@ package com.example.imc_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
        Log.w("lifecycle","CREATE - estou criando a tela")
-        finish()
+        setListeners()
+
+    }
+
+    private fun setListeners() {
+        edtAltura.doAfterTextChanged { text ->
+            //
+        }
+        edtPeso.doOnTextChanged{text, start, before, count ->
+            //txtIMC.text = text
+        }
+        btnCalcular.setOnClickListener {
+            calcularIMC(edtPeso.text.toString(), edtAltura.text.toString())
+        }
+    }
+
+    private fun calcularIMC(peso: String, altura: String) {
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+        if (peso != null && altura != null){
+            val imc = peso/(altura*altura)
+            txtIMC.text = "Seu IMC é:\n %.2f".format(imc)
+        }
     }
 
     override fun onStart() {
